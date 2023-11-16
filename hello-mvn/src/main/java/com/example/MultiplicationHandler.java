f2e1dcd5d50f9f9d445be3c07442ef1ee7474cea
yandongxiao <dxyan06@gmail.com>
2023-11-16 20:35:07 +0800

    update

---
+++ hello-mvn/src/main/java/com/example/MultiplicationHandler.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import tutorial.MultiplicationService;
+
+public class MultiplicationHandler implements MultiplicationService.Iface {
+
+    @Override
+    public int multiply(int n1, int n2) {
+        System.out.println("Multiply(" + n1 + "," + n2 + ")");
+        return n1 * n2;
+    }
+}
